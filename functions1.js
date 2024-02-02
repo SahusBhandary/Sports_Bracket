638ba5956459e3ceb981fe01163cbd9726c25d35
Meshane Peiris <meshanepeiris@gmail.com>
2024-02-02 17:10:51 -0500

    Font changes, color changes, and input is read, but nothing happens with info yet

---
+++ functions1.js
@@ -0,0 +1,4 @@
+function getTournamentInfo() {
+    const tournament_name = document.getElementById('tournament-name').value;
+    const participants = document.getElementById('participants-num').value;
+}
